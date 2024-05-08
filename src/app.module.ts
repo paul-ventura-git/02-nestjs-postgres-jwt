b35dee577f9fc35a19d0c19cf794dab67b1d72fd
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-08 15:49:24 -0500

    Connected to Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [],
 })
 export class AppModule {}
